3c6ba2720f4d6744f9b9113613b39b49d7a1dba2
Abcapon <abcapon89@gmail.com>
2023-10-27 18:05:53 +0200

    Server pronto per fare routes

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require(`express`);
 const mongoose = require(`mongoose`);
 
+const cors = require(`cors`);
+
+require(`dotenv`).config();
+
 const PORT = 5050;
 
 const app = express();
+
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on(`error`, console.error.bind(console, `Error during db connection`));
+db.once(`open`, () => {
+	console.log(`Database successfully connected!`);
+});
 
 app.listen(PORT, () => console.log(`server up and running on port ${PORT}`));
