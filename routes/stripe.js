c64371e72e74e478d12fa1cdfedeb735135fd5f4
Abcapon <abcapon89@gmail.com>
2023-11-08 20:47:21 +0100

    avanzamento stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,47 +1,35 @@
 const express = require("express");
 const stripe = express.Router();
 require("dotenv").config();
-const stripeKey = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-function calculateTotal(cartItems) {
-	let total = 0;
-	for (const item of cartItems) {
-		total += item.price;
-	}
-	return total;
-}
+const stripeKey = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 stripe.post("/create-checkout-session", async (req, res) => {
 	const { cartItems } = req.body;
+	console.log("Dati del carrello:", cartItems);
 
-	const cartTotal = calculateTotal(cartItems);
+	const line_items = cartItems.map((item) => {
+		return {
+			price_data: {
+				currency: "eur",
+				product_data: {
+					name: item.name,
+				},
+				unit_amount: item.price * 100,
+			},
+			quantity: item.quantity,
+		};
+	});
 
-	try {
-		const session = await stripeKey.checkout.sessions.create({
-			payment_method_types: ["card"],
-			line_items: cartItems.map((item) => {
-				return {
-					price_data: {
-						currency: "eur",
-						product_data: {
-							name: item.name,
-						},
-						unit_amount: item.price * 100000,
-					},
-					quantity: 1,
-				};
-			}),
-			mode: "payment",
-			success_url: "/payment/success?total=" + cartTotal,
-			cancel_url: "/payment/cancel",
-		});
+	const session = await stripeKey.checkout.sessions.create({
+		line_items,
+		mode: "payment",
+		ui_mode: "embedded",
+		return_url:
+			"https://example.com/checkout/return?session_id={CHECKOUT_SESSION_ID}",
+	});
 
-		res.json({ id: session.id });
-	} catch (error) {
-		res.status(500).json({
-			error: "Errore durante la creazione della sessione di checkout",
-		});
-	}
+	res.send({ clientSecret: session.client_secret });
 });
 
 module.exports = stripe;
